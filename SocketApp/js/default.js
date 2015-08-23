9c121550280f4a2576f1e164db722440420e44d3
OOCAMPO <oocampo@outlook.com>
2015-08-23 18:41:59 -0500

    Sample Created

--- SocketApp/js/default.js
+++ SocketApp/js/default.js
@@ -1,32 +1,77 @@
-﻿// For an introduction to the Blank template, see the following documentation:
-// http://go.microsoft.com/fwlink/?LinkId=232509
+﻿//// Copyright (c) Microsoft Corporation. All rights reserved
+
 (function () {
     "use strict";
 
     var app = WinJS.Application;
-    var activation = Windows.ApplicationModel.Activation;
+    var nav = WinJS.Navigation;
+    var activationKinds = Windows.ApplicationModel.Activation.ActivationKind;
 
-    app.onactivated = function (args) {
-        if (args.detail.kind === activation.ActivationKind.launch) {
-            if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
-                // TODO: This application has been newly launched. Initialize
-                // your application here.
-            } else {
-                // TODO: This application has been reactivated from suspension.
-                // Restore application state here.
-            }
-            args.setPromise(WinJS.UI.processAll());
+    function activated(eventObject) {
+        var activationKind = eventObject.detail.kind;
+        var activatedEventArgs = eventObject.detail.detail;
+
+        switch (activationKind) {
+            case activationKinds.launch:
+                var p = WinJS.UI.processAll().
+                    then(function () {
+
+                        // Navigate to either the first scenario or to the last running scenario
+                        // before suspension or termination.
+                        var url = SdkSample.scenarios.getAt(0).url;
+                        var initialState = {};
+                        var navHistory = app.sessionState.navigationHistory;
+                        if (navHistory) {
+                            nav.history = navHistory;
+                            url = navHistory.current.location;
+                            initialState = navHistory.current.state || initialState;
+                        }
+                        initialState.activationKind = activationKind;
+                        initialState.activatedEventArgs = activatedEventArgs;
+                        nav.history.current.initialPlaceholder = true;
+                        return nav.navigate(url, initialState);
+                    });
+
+                // Calling done on a promise chain allows unhandled exceptions to propagate.
+                p.done();
+
+                // Use setPromise to indicate to the system that the splash screen must not be torn down
+                // until after processAll and navigate complete asynchronously.
+                eventObject.setPromise(p);
+                break;
+
+            default:
+                break;
         }
-    };
+    }
 
-    app.oncheckpoint = function (args) {
-        // TODO: This application is about to be suspended. Save any state
-        // that needs to persist across suspensions here. You might use the
-        // WinJS.Application.sessionState object, which is automatically
-        // saved and restored across suspension. If you need to complete an
-        // asynchronous operation before your application is suspended, call
-        // args.setPromise().
-    };
+    function navigating(eventObject) {
+        var url = eventObject.detail.location;
+        var host = document.getElementById("contentHost");
+        // Call unload and dispose methods on current scenario, if any exist
+        if (host.winControl) {
+            host.winControl.unload && host.winControl.unload();
+            host.winControl.dispose && host.winControl.dispose();
+        }
+        WinJS.Utilities.disposeSubTree(host);
+        WinJS.Utilities.empty(host);
+        WinJS.log && WinJS.log("", "", "status");
 
+        var p = WinJS.UI.Pages.render(url, host, eventObject.detail.state).
+            then(function () {
+                var navHistory = nav.history;
+                app.sessionState.navigationHistory = {
+                    backStack: navHistory.backStack.slice(0),
+                    forwardStack: navHistory.forwardStack.slice(0),
+                    current: navHistory.current
+                };
+                app.sessionState.lastUrl = url;
+            });
+        p.done();
+        eventObject.detail.setPromise(p);
+    }
+
+    nav.addEventListener("navigating", navigating);
+    app.addEventListener("activated", activated, false);
     app.start();
 })();
